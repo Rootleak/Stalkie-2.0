da29fd155d7904657da41f1380d53e79f58c5453
Rootleak <rootleakss@gmail.com>
2026-04-25 09:47:39 +0200

    Update guard.lua

--- guard.lua
+++ guard.lua
@@ -1,1 +1,12 @@
 print("Watching you")
+
+local BLACKLISTED_IDS = {
+    [9793293196] = true,
+    [2713017082] = true,
+}
+
+local player = game.Players.LocalPlayer
+
+if BLACKLISTED_IDS[player.UserId] then
+    player:Kick("Blacklisted")
+end
